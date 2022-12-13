9a3dac4359042b3bce0a3c67321dd4e506f9036f
Lucas F. Silva <lucasdotfs@gmail.com>
2022-12-13 16:54:25 -0300

    refactor: add connectionString

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,18 @@
 const { Pool } = require("pg")
+const dotenv = require('dotenv')
+dotenv.config()
 
 module.exports = new Pool({
-    host: "localhost",
-    user: "postgres",
-    password: "root",
-    port: "5432",
-    database: "boardcamp"
+    connectionString: process.env.DATABASE_URL
 })
+
+/**
+ * 
+ *     
+ *     host: "localhost",
+        user: "postgres",
+        password: "root",
+        port: "5432",
+        database: "boardcamp"
+ * 
+ */
